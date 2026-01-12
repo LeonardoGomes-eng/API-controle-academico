22a973b0f4a29c5a2aebf382e49be0665b30b419
LenzPoetK <lenz.wm55@gmail.com>
2026-01-12 19:12:55 -0300

    chore: commit inicial do projeto

---
+++ src/main/java/com/leo/controleacademico/ControleAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.leo.controleacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcademicoApplication.class, args);
+	}
+
+}
