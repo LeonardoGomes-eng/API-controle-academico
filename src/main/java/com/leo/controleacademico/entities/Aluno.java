9f18a27a499359227349466dc119f36af62c092a
LenzPoetK <lenz.wm55@gmail.com>
2026-01-14 00:30:56 -0300

    chore(aluno): adicionados getters, setters e construtor nao parametrizado na entidade

--- src/main/java/com/leo/controleacademico/entities/Aluno.java
+++ src/main/java/com/leo/controleacademico/entities/Aluno.java
@@ -14,4 +14,39 @@ public class Aluno {
     private String nome;
     private int matricula;
     private String curso;
+
+    public Aluno(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
