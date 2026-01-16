9b5957e922368b6a2c3a37fa0199027067666cc8
LenzPoetK <lenz.wm55@gmail.com>
2026-01-16 15:52:43 -0300

    feat(disciplina): criado DisciplinaRepository

---
+++ src/main/java/com/leo/controleacademico/repositories/DisciplinaRepository.java
@@ -0,0 +1,7 @@
+package com.leo.controleacademico.repositories;
+
+import com.leo.controleacademico.entities.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+}
