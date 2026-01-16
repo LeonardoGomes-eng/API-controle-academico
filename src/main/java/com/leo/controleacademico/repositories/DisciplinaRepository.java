60c1f71afad787f37bfcfc2c13ba41c9262e9ae8
LenzPoetK <lenz.wm55@gmail.com>
2026-01-16 15:57:32 -0300

    fix: adicionado @Repository em DisciplinaRepository

--- src/main/java/com/leo/controleacademico/repositories/DisciplinaRepository.java
+++ src/main/java/com/leo/controleacademico/repositories/DisciplinaRepository.java
@@ -2,6 +2,8 @@ package com.leo.controleacademico.repositories;
 
 import com.leo.controleacademico.entities.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
 }
